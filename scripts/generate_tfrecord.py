be3cca060d1d4befab59d039b1aa8504538cecc1
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2021-06-21 20:37:55 +0700

    solve error

--- scripts/generate_tfrecord.py
+++ scripts/generate_tfrecord.py
@@ -110,7 +110,7 @@ def split(df, group):
 def create_tf_example(group, path):
     ENCODED_JPG = [1]
     with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
-    encoded_jpg = ENCODED_JPG[-1]
+        encoded_jpg = ENCODED_JPG[-1]
         try:
             encoded_jpg = fid.read()
             ENCODED_JPG.append(encoded_jpg)
